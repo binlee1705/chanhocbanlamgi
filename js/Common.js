ca78a24928c929daae96ed9ef67ef0220a41056d
binlee1705 <kunbin.jack@gmail.com>
2022-11-25 01:43:51 +0700

    abc

---
+++ js/Common.js
@@ -0,0 +1,110 @@
+﻿//"use strict"; 
+function owlslide(num, margin, autoplay, dot, nav, items, animateIn, animateOut) {
+    var option = {
+        items: num,
+        autoplay: num > items[0] ? autoplay : false,
+        autoplayTimeout: 5000,
+        smartSpeed: 1500,
+        loop: num > 1,
+        nav: num > items[0] ? nav : false,
+        dots: num > items[0] ? dot : false,
+        autoplayHoverPause: true,
+        margin: margin[0],
+        lazyLoad: true,
+        navText: [''],
+        animateIn: animateIn,
+        animateOut: animateOut,
+        responsive: {
+            0: {
+                items: items[4],
+                margin: margin[4],
+                autoplay: num > items[4] ? autoplay : false,
+                nav: num > items[4] ? nav : false,
+                dots: num > items[4] ? dot : false,
+            },
+            479: {
+                items: items[3],
+                margin: margin[3],
+                autoplay: num > items[3] ? autoplay : false,
+                nav: num > items[3] ? nav : false,
+                dots: num > items[3] ? dot : false,
+            },
+            767: {
+                items: items[2],
+                margin: margin[2],
+                autoplay: num > items[2] ? autoplay : false,
+                nav: num > items[2] ? nav : false,
+                dots: num > items[2] ? dot : false,
+            },
+            991: {
+                items: items[1],
+                margin: margin[1],
+                autoplay: num > items[1] ? autoplay : false,
+                nav: num > items[1] ? nav : false,
+                dots: num > items[1] ? dot : false,
+            },
+            1199: {
+                items: items[0],
+                margin: margin[0],
+                autoplay: num > items[0] ? autoplay : false,
+                nav: num > items[0] ? nav : false,
+                dots: num > items[0] ? dot : false,
+            }
+        }
+    }
+    return option;
+}
+
+$(document).ready(() => {
+    $('html').on('click', ".tabs .tab-links a",
+        function (e) {
+            var currentAttrValue = $(this).attr('href');
+            $('.tabs ' + currentAttrValue).addClass("active").siblings().removeClass("active");
+            $(this).parent('li').addClass('active').siblings().removeClass('active');
+            e.preventDefault();
+        });
+    // $(".marquee").marquee({ duration: 20000, gap: 20, delayBeforeStart: 0, direction: "left", duplicated: true, pauseOnHover: true });
+
+    $('#slide .group').each(function () {
+        $(this).owlCarousel(owlslide($(this).find('>*').size(), [0, 0, 0, 0, 0, 0], true, true, true, [1, 1, 1, 1, 1], "fadeIn", "fadeOut"));
+    });
+    $('#gallery .group').each(function () {
+        $(this).owlCarousel(owlslide($(this).find('>*').size(), [20, 20, 20, 20, 20, 20], true, true, true, [4, 4, 4, 4, 4], "", ""));
+    });
+    $("#lstQA .ques").click(function () {
+        if (!$(this).parents(".item").hasClass("ac")) {
+            $("#lstQA .item").removeClass("ac")
+            $("#lstQA .item .ans").slideUp();
+            $(this).parents(".item").addClass("ac");
+            $(this).next(".ans").slideDown();
+        }
+    })
+});
+
+
+var size = parseInt($(".noidung").css("font-size")),
+    lineheight = parseInt($(".noidung").css("line-height"));
+size || (size = 16);
+lineheight || (lineheight = 24);
+
+function Increasenoidung() {
+    size++;
+    lineheight += 2;
+    $(".noidung").css("cssText", "font-size:" + size + "px !important; line-height:" + lineheight + "px !important");
+    $(".noidung").find("*").css("cssText",
+        "font-size:" + size + "px !important; line-height:" + lineheight + "px !important");
+}
+function Decreasenoidung() {
+    size--;
+    lineheight -= 2;
+    $(".noidung").css("cssText", "font-size:" + size + "px !important; line-height:" + lineheight + "px !important");
+    $(".noidung").find("*").css("cssText",
+        "font-size:" + size + "px !important; line-height:" + lineheight + "px !important");
+}
+function Resetnoidung() {
+    size = 16;
+    lineheight = 24;
+    $(".noidung").css("cssText", "font-size:" + size + "px !important; line-height:" + lineheight + "px !important");
+    $(".noidung").find("*").css("cssText",
+        "font-size:" + size + "px !important; line-height:" + lineheight + "px !important");
+}
---
+++ js/getData.js
@@ -0,0 +1,111 @@
+const dataNews = [
+    {
+        "Id": 1,
+        "Title": "Nga tuyên bố không bán dầu, khí đốt cho nước áp giá trần",
+        "Image": "https://i1-vnexpress.vnecdn.net/2022/11/24/afp-com-20211223-partners-033-2603-2554-1669299412.jpg?w=220&h=132&q=100&dpr=1&fit=crop&s=II-BM6r9KsqOsTcyXJ-JhQ",
+        "Description": "Nga tuyên bố không cung cấp dầu, khí đốt cho các nước ủng hộ áp giá trần với dầu Nga, nhưng sẽ quyết định sau khi phân tích số liệu.",
+        "Content": "<p>Nga tuy&ecirc;n bố kh&ocirc;ng cung cấp dầu, kh&iacute; đốt cho c&aacute;c nước ủng hộ &aacute;p gi&aacute; trần với dầu Nga, nhưng sẽ quyết định sau khi ph&acirc;n t&iacute;ch số liệu.</p> <p>&quot;Hiện ch&uacute;ng t&ocirc;i thực hiện theo lập trường của Tổng thống Vladimir Putin rằng sẽ kh&ocirc;ng cung cấp dầu v&agrave; kh&iacute; đốt cho những quốc gia sẽ &aacute;p đặt mức gi&aacute; trần&quot;, ph&aacute;t ng&ocirc;n vi&ecirc;n Điện Kremlin n&oacute;i trong cuộc họp b&aacute;o tại Moskva h&ocirc;m nay.</p> <p>&quot;Người ch&acirc;u &Acirc;u vẫn c&oacute; những cuộc thảo luận rất kh&oacute; hiểu về mức gi&aacute; trần n&agrave;y. C&oacute; cảm gi&aacute;c như họ chỉ đang cố đưa ra quyết định, kh&ocirc;ng phải để c&oacute; hiệu lực, m&agrave; chỉ để thể hiện rằng giới hạn đ&atilde; được đưa ra&quot;, &ocirc;ng Peskov n&oacute;i th&ecirc;m. &quot;Tuy nhi&ecirc;n, ch&uacute;ng t&ocirc;i cần ph&acirc;n t&iacute;ch c&aacute;c số liệu được thảo luận ở ch&acirc;u &Acirc;u trước khi x&aacute;c định r&otilde; quan điểm của m&igrave;nh&quot;.</p> <p>B&igrave;nh luận của &ocirc;ng Peskov được đưa ra trong bối cảnh G7 đang xem x&eacute;t mức trần đối với dầu được vận chuyển bằng đường biển của Nga ở mức 65-70 USD/th&ugrave;ng, d&ugrave; c&aacute;c ch&iacute;nh phủ Li&ecirc;n minh ch&acirc;u &Acirc;u (EU) vẫn chưa thống nhất về mức gi&aacute; v&agrave; c&aacute;c cuộc đ&agrave;m ph&aacute;n sẽ tiếp tục.</p> <p><img alt=\"Người phát ngôn Điện Kremlin Dmitry Peskov tại Moskva hồi tháng 12/2021. Ảnh: AFP.\" data-ll-status=\"loaded\" data-src=\"https://i1-vnexpress.vnecdn.net/2022/11/24/afp-com-20211223-partners-033-9488-9216-1669299412.jpg?w=680&amp;h=0&amp;q=100&amp;dpr=1&amp;fit=crop&amp;s=pdefGjeyh_qX6B_M4US4sA\" intrinsicsize=\"680x0\" itemprop=\"contentUrl\" loading=\"lazy\" src=\"https://i1-vnexpress.vnecdn.net/2022/11/24/afp-com-20211223-partners-033-9488-9216-1669299412.jpg?w=680&amp;h=0&amp;q=100&amp;dpr=1&amp;fit=crop&amp;s=pdefGjeyh_qX6B_M4US4sA\" /></p> <p><iframe frameborder=\"0\" height=\"0\" id=\"google_ads_iframe_/27973503/Vnexpress/Desktop/Inimage/Thegioi/Thegioi.detail_0\" name=\"\" scrolling=\"no\" src=\"https://0f55d39c5d3e9ae3bfa27ad30d152ff8.safeframe.googlesyndication.com/safeframe/1-0-40/html/container.html\" title=\"3rd party ad content\" width=\"0\"></iframe></p> <p>Người ph&aacute;t ng&ocirc;n Điện Kremlin Dmitry Peskov tại Moskva hồi th&aacute;ng 12/2021. Ảnh:&nbsp;<em>AFP</em>.</p> <p>Một quan chức cấp cao Bộ T&agrave;i ch&iacute;nh Mỹ ng&agrave;y 22/11 n&oacute;i rằng EU đang tham khảo &yacute; kiến c&aacute;c th&agrave;nh vi&ecirc;n về mức gi&aacute; v&agrave; phương T&acirc;y sẽ thực hiện c&aacute;c bước &aacute;p gi&aacute; trần ngay sau khi quy tr&igrave;nh của EU ho&agrave;n tất. Gi&aacute; trần sẽ l&agrave; cơ sở cho lệnh cấm&nbsp;<a data-itm-added=\"1\" data-itm-source=\"#vn_source=Detail-TheGioi-4540177&amp;vn_campaign=Box-InternalLink&amp;vn_medium=Link-CoHieuLucTuNgay5/12&amp;vn_term=Desktop&amp;vn_thumb=0\" href=\"https://vnexpress.net/phuong-tay-sap-ap-tran-gia-dau-nga-4539350.html\" rel=\"dofollow\">c&oacute; hiệu lực từ ng&agrave;y 5/12</a>, cấm c&aacute;c c&ocirc;ng ty vận chuyển hoặc cung cấp bảo hiểm cho c&aacute;c l&ocirc; h&agrave;ng dầu Nga được b&aacute;n tr&ecirc;n mức gi&aacute; cố định.</p> <p>Đ&acirc;y l&agrave; lần đầu ti&ecirc;n Mỹ v&agrave; c&aacute;c đồng minh phương T&acirc;y vạch lộ tr&igrave;nh cụ thể cho biện ph&aacute;p &aacute;p trần gi&aacute; dầu Nga, sau nhiều th&aacute;ng thảo luận.</p> <p>Ngoại trưởng Hungary Peter Szijjarto h&ocirc;m nay cho biết nước n&agrave;y được miễn khỏi đề xuất &aacute;p gi&aacute; trần dầu Nga, đồng nghĩa Hungary kh&ocirc;ng chịu ảnh hưởng nếu mức trần đề xuất được th&ocirc;ng qua.</p> <p>Thủ tướng Hungary Viktor Orban cuối th&aacute;ng 9 n&oacute;i rằng c&aacute;c lệnh trừng phạt nhằm v&agrave;o Nga của EU n&ecirc;n được gỡ bỏ, do ch&uacute;ng l&agrave;m tăng gi&aacute; kh&iacute; đốt v&agrave; lạm ph&aacute;t. &Ocirc;ng Orban được coi l&agrave; l&atilde;nh đạo ủng hộ Nga nhất trong 27 nước th&agrave;nh vi&ecirc;n EU khi nhiều lần n&oacute;i li&ecirc;n minh tự hủy hoại ch&iacute;nh m&igrave;nh v&igrave; &aacute;p c&aacute;c lệnh trừng phạt với Moskva, hay cảnh b&aacute;o c&aacute;c biện ph&aacute;p n&agrave;y c&oacute; nguy cơ ph&aacute; hủy kinh tế ch&acirc;u &Acirc;u.</p> <p>Cuộc&nbsp;<a data-itm-added=\"1\" data-itm-source=\"#vn_source=Detail-TheGioi-4540177&amp;vn_campaign=Box-InternalLink&amp;vn_medium=Link-XungDotUkraine&amp;vn_term=Desktop&amp;vn_thumb=0\" href=\"https://vnexpress.net/chu-de/chien-su-nga-ukraine-4570\" rel=\"dofollow\">xung đột Ukraine</a>, b&ugrave;ng ph&aacute;t ng&agrave;y 24/2, đ&atilde; khiến gi&aacute; năng lượng tăng vọt, mang lại nguồn lợi nhuận khổng lồ cho Nga v&agrave; gi&uacute;p nước n&agrave;y giảm thiểu c&aacute;c t&aacute;c động của biện ph&aacute;p trừng phạt m&agrave; phương T&acirc;y &aacute;p đặt. Mỹ v&agrave; đồng minh cho biết &aacute;p trần gi&aacute; dầu kh&ocirc;ng chỉ nhằm tước nguồn thu của Nga, m&agrave; c&ograve;n gi&uacute;p hạ nhiệt gi&aacute; năng lượng to&agrave;n cầu.</p> <p>Nga nhiều lần cảnh b&aacute;o nước n&agrave;y c&oacute; thể cắt giảm sản lượng dầu để đối ph&oacute; với t&aacute;c động ti&ecirc;u cực nếu phương T&acirc;y &aacute;p gi&aacute; trần. Điện Kremlin cũng tuy&ecirc;n bố sẽ ngừng b&aacute;n dầu cho c&aacute;c nước &aacute;p gi&aacute; trần.</p> <p>Trước khi Nga mở chiến dịch qu&acirc;n sự tại&nbsp;<a data-itm-added=\"1\" data-itm-source=\"#vn_source=Detail-TheGioi-4540177&amp;vn_campaign=Box-InternalLink&amp;vn_medium=Link-Ukraine&amp;vn_term=Desktop&amp;vn_thumb=0\" href=\"https://vnexpress.net/chu-de/ukraine-692\" rel=\"dofollow\">Ukraine</a>&nbsp;hồi th&aacute;ng 2, ch&acirc;u &Acirc;u l&agrave; điểm đến của gần một nửa lượng dầu xuất khẩu Nga. Năm 2021, khối n&agrave;y nhập từ Nga khoảng 2,2 triệu th&ugrave;ng dầu th&ocirc;, 1,2 triệu th&ugrave;ng sản phẩm tinh chế v&agrave; 0,5 triệu th&ugrave;ng dầu diesel mỗi ng&agrave;y, trong đ&oacute; Đức, Ba Lan v&agrave; H&agrave; Lan l&agrave; những kh&aacute;ch h&agrave;ng lớn nhất.</p>",
+        "CreatedDate": "25/11/2022"
+    },
+    {
+        "Id": 2,
+        "Title": "Thụy Sĩ thắng nhờ tiền đạo gốc Cameroon",
+        "Image": "https://i1-thethao.vnecdn.net/2022/11/24/3246-jpeg-8793-1669294230-1669-7998-6882-1669298005.jpg?w=220&h=132&q=100&dpr=1&fit=crop&s=aL_oGwcmpyBCEknf-GbcXw",
+        "Description": "Trong trận mở màn bảng G World Cup 2022 chiều 24/11, Breel Embolo ghi bàn giúp Thụy Sĩ thắng đội tuyển quê hương Cameroon 1-0. ",
+        "Content": "sdsdfgsdfgsdfg",
+        "CreatedDate": "25/11/2022"
+    },
+    {
+        "Id": 3,
+        "Title": "Mây thấu kính bao đỉnh núi Bà Đen như đĩa bay",
+        "Image": "https://i1-vnexpress.vnecdn.net/2022/11/24/316318298896468949354495240754-9642-6129-1669275778.jpg?w=220&h=132&q=100&dpr=1&fit=crop&s=QjQfkFZ9lgA33oNX7FX2Xg",
+        "Description": "Những đám mây lạ xuất hiện ở núi Bà Đen được chuyên gia lý giải là hiện tượng mây thấu kính.",
+        "Content": "sdsdfgsdfgsdfg",
+        "CreatedDate": "25/11/2022"
+    },
+    {
+        "Id": 4,
+        "Title": "Hyundai Tucson áp đảo bình chọn crossover cỡ C của năm",
+        "Image": "https://i1-vnexpress.vnecdn.net/2022/11/23/HyundaiTucson1-1669173513-9638-1669173598.jpg?w=220&h=132&q=100&dpr=1&fit=crop&s=yP0OOMQ7G8oRBULZ4o7XVQ",
+        "Description": "Hyundai Tucson có lượng bình chọn cao gấp hai lần đối thủ đứng thứ hai, cũng là một mẫu xe Hàn, Kia Sportage.",
+        "Content": "sdsdfgsdfgsdfg",
+        "CreatedDate": "25/11/2022"
+    },
+    {
+        "Id": 5,
+        "Title": "Brazil - Serbia: Phô trương sức mạnh",
+        "Image": "https://i1-thethao.vnecdn.net/2022/11/24/qjypgykvpjfs7lfcdu4dv23jbu-jpe-7825-6806-1669276923.jpg?w=220&h=132&q=100&dpr=1&fit=crop&s=bpyZutS2ETdkZJKZ1D9kMw",
+        "Description": "Hàng công toàn sao của Brazil có cơ hội để thị uy sức mạnh ở lượt ra quân bảng G World Cup 2022 gặp Serbia hôm nay. ",
+        "Content": "sdsdfgsdfgsdfg",
+        "CreatedDate": "25/11/2022"
+    },
+]
+
+const dataQA = [
+    {
+        "Id": 1,
+        "Question": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae quaerat voluptates aliquam obcaecati ad ullam enim illum mollitia. Quos, quis.",
+        "Answer": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae possimus suscipit sequi perferendis iste doloremque magnam amet nam dignissimos quos eligendi, fugit exercitationem assumenda optio enim explicabo totam distinctio, atque officiis, animi alias! Aspernatur praesentium unde cum id explicabo dolorem, veniam illum, nam sapiente vero eveniet expedita magnam natus earum? Quasi, a necessitatibus? Recusandae, amet fugiat aliquid officiis debitis dolore facilis deserunt a odit? Maxime dolores totam magni quidem rerum dolore inventore explicabo veritatis enim voluptates, accusamus nemo, architecto repellendus provident, ducimus asperiores at quasi ex molestiae dicta itaque expedita eveniet quas! Tempore, tenetur, quas veniam iure rem debitis odio, rerum facere quisquam unde ad? Rem in repudiandae magnam excepturi dignissimos sed eveniet fugit ratione inventore maxime adipisci, tempore laudantium, iure aliquid ad esse. Itaque tenetur saepe quidem! Animi eos minus facere quibusdam nulla voluptate architecto, fugiat obcaecati, molestiae quis blanditiis labore dolore earum laudantium dignissimos provident temporibus et sint."
+    },
+    {
+        "Id": 2,
+        "Question": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae quaerat voluptates aliquam obcaecati ad ullam enim illum mollitia. Quos, quis.",
+        "Answer": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae possimus suscipit sequi perferendis iste doloremque magnam amet nam dignissimos quos eligendi, fugit exercitationem assumenda optio enim explicabo totam distinctio, atque officiis, animi alias! Aspernatur praesentium unde cum id explicabo dolorem, veniam illum, nam sapiente vero eveniet expedita magnam natus earum? Quasi, a necessitatibus? Recusandae, amet fugiat aliquid officiis debitis dolore facilis deserunt a odit? Maxime dolores totam magni quidem rerum dolore inventore explicabo veritatis enim voluptates, accusamus nemo, architecto repellendus provident, ducimus asperiores at quasi ex molestiae dicta itaque expedita eveniet quas! Tempore, tenetur, quas veniam iure rem debitis odio, rerum facere quisquam unde ad? Rem in repudiandae magnam excepturi dignissimos sed eveniet fugit ratione inventore maxime adipisci, tempore laudantium, iure aliquid ad esse. Itaque tenetur saepe quidem! Animi eos minus facere quibusdam nulla voluptate architecto, fugiat obcaecati, molestiae quis blanditiis labore dolore earum laudantium dignissimos provident temporibus et sint."
+    },
+    {
+        "Id": 3,
+        "Question": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae quaerat voluptates aliquam obcaecati ad ullam enim illum mollitia. Quos, quis.",
+        "Answer": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae possimus suscipit sequi perferendis iste doloremque magnam amet nam dignissimos quos eligendi, fugit exercitationem assumenda optio enim explicabo totam distinctio, atque officiis, animi alias! Aspernatur praesentium unde cum id explicabo dolorem, veniam illum, nam sapiente vero eveniet expedita magnam natus earum? Quasi, a necessitatibus? Recusandae, amet fugiat aliquid officiis debitis dolore facilis deserunt a odit? Maxime dolores totam magni quidem rerum dolore inventore explicabo veritatis enim voluptates, accusamus nemo, architecto repellendus provident, ducimus asperiores at quasi ex molestiae dicta itaque expedita eveniet quas! Tempore, tenetur, quas veniam iure rem debitis odio, rerum facere quisquam unde ad? Rem in repudiandae magnam excepturi dignissimos sed eveniet fugit ratione inventore maxime adipisci, tempore laudantium, iure aliquid ad esse. Itaque tenetur saepe quidem! Animi eos minus facere quibusdam nulla voluptate architecto, fugiat obcaecati, molestiae quis blanditiis labore dolore earum laudantium dignissimos provident temporibus et sint."
+    },
+    {
+        "Id": 4,
+        "Question": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae quaerat voluptates aliquam obcaecati ad ullam enim illum mollitia. Quos, quis.",
+        "Answer": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae possimus suscipit sequi perferendis iste doloremque magnam amet nam dignissimos quos eligendi, fugit exercitationem assumenda optio enim explicabo totam distinctio, atque officiis, animi alias! Aspernatur praesentium unde cum id explicabo dolorem, veniam illum, nam sapiente vero eveniet expedita magnam natus earum? Quasi, a necessitatibus? Recusandae, amet fugiat aliquid officiis debitis dolore facilis deserunt a odit? Maxime dolores totam magni quidem rerum dolore inventore explicabo veritatis enim voluptates, accusamus nemo, architecto repellendus provident, ducimus asperiores at quasi ex molestiae dicta itaque expedita eveniet quas! Tempore, tenetur, quas veniam iure rem debitis odio, rerum facere quisquam unde ad? Rem in repudiandae magnam excepturi dignissimos sed eveniet fugit ratione inventore maxime adipisci, tempore laudantium, iure aliquid ad esse. Itaque tenetur saepe quidem! Animi eos minus facere quibusdam nulla voluptate architecto, fugiat obcaecati, molestiae quis blanditiis labore dolore earum laudantium dignissimos provident temporibus et sint."
+    },
+]
+
+const gallery = [
+    {
+        "Id": 1,
+        "Url": "http://www.anhngu.usc.edu.vn/Data/Sites/1/News/4306/190731_chanta2.jpg",
+        "Title": "abc",
+    },
+    {
+        "Id": 2,
+        "Url": "http://www.anhngu.usc.edu.vn/Data/Sites/1/News/4306/190731_chanta2.jpg",
+        "Title": "abc",
+    },
+    {
+        "Id": 3,
+        "Url": "http://www.anhngu.usc.edu.vn/Data/Sites/1/News/4306/190731_chanta2.jpg",
+        "Title": "abc",
+    },
+    {
+        "Id": 4,
+        "Url": "http://www.anhngu.usc.edu.vn/Data/Sites/1/News/4306/190731_chanta2.jpg",
+        "Title": "abc",
+    },
+    {
+        "Id": 5,
+        "Url": "http://www.anhngu.usc.edu.vn/Data/Sites/1/News/4306/190731_chanta2.jpg",
+        "Title": "abc",
+    },
+]
+
+const slide = [
+    {
+        "Id": 1,
+        "Url": "https://static2.yan.vn/YanNews/2167221/201801/20180109-010716-loikhuyensaunhunggiohoctapueoai.jpg",
+        "Title": "abc",
+    },
+    {
+        "Id": 2,
+        "Url": "https://i.ytimg.com/vi/RWLyaTpw1VY/maxresdefault.jpg",
+        "Title": "abc",
+    },
+    {
+        "Id": 3,
+        "Url": "http://www.anhngu.usc.edu.vn/Data/Sites/1/News/4306/190731_chanta2.jpg",
+        "Title": "abc",
+    },
+]
